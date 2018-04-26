866bff46ec0b149e6b28c6a14f430c87b5d65c64
Vijay <vijaysarathi947@gmail.com>
2018-04-26 16:01:19 +0530

    First Commit
    New Commit

---
+++ src/main/java/com/BillPayment/BillPaymentSystemApplication.java
@@ -0,0 +1,12 @@
+package com.BillPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillPaymentSystemApplication.class, args);
+	}
+}
